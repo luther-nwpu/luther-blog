e6936a9b3c506b34327295665dfec9237c372007
e <luthernpu@gmail.com>
2019-01-24 11:38:14 +0800

    d

--- blog-api/src/routes/admin.ts
+++ blog-api/src/routes/admin.ts
@@ -6,15 +6,16 @@ const router = new koaRouter()
 router.prefix('/admin')
 
 router.post('/upload', async (ctx, next) => {
-    await uploadFile(ctx, {
-        fileType: 'album', // common or album
-        path: 'public/'
-    })
-    return ctx.body = { 'text': '上传成功' }
+    const file = ctx.request.files.file    // 获取上传文件
+    console.log(file)
+    const reader = fs.createReadStream(file.path)    // 创建可读流
+    const ext = file.name.split('.').pop()        // 获取上传文件扩展名
+    const upStream = fs.createWriteStream(`upload/666.${ext}`)        // 创建可写流
+    reader.pipe(upStream)    // 可读流通过管道写入可写流
+    return ctx.body = '上传成功'
 })
 
 router.post('/commitArticle', async (ctx, next) => {
-    console.log('ddddddddddddddddddddd')
     return ctx.body = { 'text': '你好' }
 })
 
